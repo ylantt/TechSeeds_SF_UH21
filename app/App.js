c6a5de9a4544c95d22eae512758763d61fffbe0a
Luu Van Cui <luuvancui10a1516@gmail.com>
2021-09-22 15:18:44 +0700

    .

--- app/App.js
+++ app/App.js
@@ -9,6 +9,7 @@ import CameraScreen from "./src/screens/CameraScreen";
 import EvaluateImgScreen from "./src/screens/EvaluateImgScreen";
 import DoctorListScreen from "./src/screens/DoctorListScreen";
 import DoctorDetailScreen from "./src/screens/DoctorDetailScreen";
+import ChatScreen from "./src/screens/ChatScreen";
 
 const navigator = createStackNavigator(
   {
@@ -21,6 +22,7 @@ const navigator = createStackNavigator(
     EvaluateImg: EvaluateImgScreen,
     DoctorList: DoctorListScreen,
     DoctorDetail: DoctorDetailScreen,
+    Chat: ChatScreen,
   },
   {
     initialRouteName: "Welcome",
