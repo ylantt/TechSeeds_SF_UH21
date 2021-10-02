5020316b0fd3faf6e930c606ca668da03b5d47ed
Luu Van Cui <luuvancui10a1516@gmail.com>
2021-10-03 05:33:53 +0700

    get me and chat

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const asyncHandler = require("./async");
 const ErrorResponse = require("../untils/errorResponse");
 const User = require("../models/User");
+const Doctor = require("../models/Doctor");
 
 // Protect routes
 exports.protect = asyncHandler(async (req, res, next) => {
@@ -33,6 +34,47 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
 
     next();
+  } catch (err) {
+    return next(new ErrorResponse("Not authorize to access this route", 401));
+  }
+});
+
+exports.getUser = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(" ")[1];
+    // Set token from cookie
+  }
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  //   Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("Not authorize to access this route", 401));
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+
+    let user = await User.findById(decoded.id);
+
+    if (!user) {
+      user = await Doctor.findById(decoded.id);
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
   } catch (err) {
     return next(new ErrorResponse("Not authorize to access this route", 401));
   }
