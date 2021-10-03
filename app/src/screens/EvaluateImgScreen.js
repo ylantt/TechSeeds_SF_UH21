26c9f5cf61ad0a4de34e77eb9f5caf28d361648d
Luu Van Cui <luuvancui10a1516@gmail.com>
2021-10-03 07:34:56 +0700

    .

--- app/src/screens/EvaluateImgScreen.js
+++ app/src/screens/EvaluateImgScreen.js
@@ -13,9 +13,12 @@ const EvaluateImgScreen = ({ navigation }) => {
   const [mainReason, setMainReason] = useState("");
 
   const reason = {
-    nam_da: "Ánh nắng mặt trời, Di truyền,  Nội tiết tố, sử dụng mỹ phẩm không đúng cách, chất lượng kém, chế độ sinh hoạt không khoa học hợp lý.",
-    mun_viem_do: "Rửa mặt quá nhiều, Lạm dụng tẩy tế bào chết, Không làm sạch mồ hôi, Nặn mụn thường xuyên, Lười gội đầu, Để sản phẩm tạo kiểu tóc bết dính trên da",
-    viem_da_tiet_ba: "Do tình trạng da bị nhờn, tiết dầu nhiều, Hormon cũng ảnh hưởng đến sự phát triển của bệnh, Yếu tố di truyền cũng là nguyên nhân gây bệnh, Thay đổi nội tiết tố",
+    nam_da:
+      "Ánh nắng mặt trời, Di truyền,  Nội tiết tố, sử dụng mỹ phẩm không đúng cách, chất lượng kém, chế độ sinh hoạt không khoa học hợp lý.",
+    mun_viem_do:
+      "Rửa mặt quá nhiều, Lạm dụng tẩy tế bào chết, Không làm sạch mồ hôi, Nặn mụn thường xuyên, Lười gội đầu, Để sản phẩm tạo kiểu tóc bết dính trên da",
+    viem_da_tiet_ba:
+      "Do tình trạng da bị nhờn, tiết dầu nhiều, Hormon cũng ảnh hưởng đến sự phát triển của bệnh, Yếu tố di truyền cũng là nguyên nhân gây bệnh, Thay đổi nội tiết tố",
   };
 
   const getDataFromModel = async (photoBase64) => {
@@ -64,17 +67,22 @@ const EvaluateImgScreen = ({ navigation }) => {
           <Text style={[texts.midText, utilities.mt3]}>Đang phân tích...</Text>
         ) : name ===
           "Da của bạn khoẻ mạnh. Hãy chăm sóc da thường xuyên nhé!" ? (
-              <Text style={[texts.midText, utilities.mt3]}>{name}</Text>
-            ) : (
-              <View>
-                <Text style={[texts.midText, utilities.mt7]}>
-                  Bệnh được chuẩn đoán: {name}
-                </Text>
-                <Text style={[texts.midText]}>{`Mức độ: ${level}`}</Text>
-                <Text style={[texts.midText]}>{`Nguyên nhân phổ biến: ${mainReason}`}</Text>
-                <Text style={utilities.mt3}>Để nắm rõ hơn về tình hình loại bệnh, bạn có thể bấm qua tab tips hoặc tìm đến bác sĩ</Text>
-              </View>
-            )}
+          <Text style={[texts.midText, utilities.mt3]}>{name}</Text>
+        ) : (
+          <View>
+            <Text style={[texts.midText, utilities.mt7]}>
+              Bệnh được chuẩn đoán: {name}
+            </Text>
+            <Text style={[texts.midText]}>{`Mức độ: ${level}`}</Text>
+            <Text
+              style={{ fontSize: 14, marginTop: 10 }}
+            >{`Nguyên nhân phổ biến: ${mainReason}`}</Text>
+            <Text style={utilities.mt3}>
+              Để nắm rõ hơn về tình hình loại bệnh, bạn có thể bấm qua tab tips
+              hoặc tìm đến bác sĩ
+            </Text>
+          </View>
+        )}
       </View>
       <TouchableOpacity onPress={() => navigation.navigate("Intro")}>
         <Text style={[buttons.btn, buttons.bottomBtn, buttons.roundBtn]}>
