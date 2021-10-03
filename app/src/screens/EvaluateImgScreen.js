b5342ddef7c8b29b74b86bb37a77ea5ddc9cc2cf
Lan <truongylan2000@gmail.com>
2021-10-03 07:12:58 +0700

    add reason info

--- app/src/screens/EvaluateImgScreen.js
+++ app/src/screens/EvaluateImgScreen.js
@@ -10,11 +10,12 @@ const EvaluateImgScreen = ({ navigation }) => {
   const [name, setName] = useState("");
   const [problem, setProblem] = useState("");
   const [level, setLevel] = useState("");
+  const [mainReason, setMainReason] = useState("");
 
   const reason = {
-    nam_da: "",
-    mun_viem_do: "",
-    viem_da_tiet_ba: "",
+    nam_da: "Ánh nắng mặt trời, Di truyền,  Nội tiết tố, sử dụng mỹ phẩm không đúng cách, chất lượng kém, chế độ sinh hoạt không khoa học hợp lý.",
+    mun_viem_do: "Rửa mặt quá nhiều, Lạm dụng tẩy tế bào chết, Không làm sạch mồ hôi, Nặn mụn thường xuyên, Lười gội đầu, Để sản phẩm tạo kiểu tóc bết dính trên da",
+    viem_da_tiet_ba: "Do tình trạng da bị nhờn, tiết dầu nhiều, Hormon cũng ảnh hưởng đến sự phát triển của bệnh, Yếu tố di truyền cũng là nguyên nhân gây bệnh, Thay đổi nội tiết tố",
   };
 
   const getDataFromModel = async (photoBase64) => {
@@ -29,12 +30,15 @@ const EvaluateImgScreen = ({ navigation }) => {
       if (data.name === "nam_da") {
         setName("Nám da");
         setProblem("MX");
+        setMainReason(reason.nam_da);
       } else if (data.name === "viem_da_tiet_ba") {
         setName("Viêm da tiết bã");
         setProblem("SD");
+        setMainReason(reason.viem_da_tiet_ba);
       } else if (data.name === "mun_viem_do") {
         setName("Mụn viêm đỏ");
         setProblem("IA");
+        setMainReason(reason.mun_viem_do);
       }
 
       if (data.level >= 0.4) {
@@ -60,15 +64,17 @@ const EvaluateImgScreen = ({ navigation }) => {
           <Text style={[texts.midText, utilities.mt3]}>Đang phân tích...</Text>
         ) : name ===
           "Da của bạn khoẻ mạnh. Hãy chăm sóc da thường xuyên nhé!" ? (
-          <Text style={[texts.midText, utilities.mt3]}>{name}</Text>
-        ) : (
-          <View>
-            <Text style={[texts.midText, utilities.mt7]}>
-              Bệnh được chuẩn đoán: {name}
-            </Text>
-            <Text style={[texts.midText]}>{`Mức độ: ${level}`}</Text>
-          </View>
-        )}
+              <Text style={[texts.midText, utilities.mt3]}>{name}</Text>
+            ) : (
+              <View>
+                <Text style={[texts.midText, utilities.mt7]}>
+                  Bệnh được chuẩn đoán: {name}
+                </Text>
+                <Text style={[texts.midText]}>{`Mức độ: ${level}`}</Text>
+                <Text style={[texts.midText]}>{`Nguyên nhân phổ biến: ${mainReason}`}</Text>
+                <Text style={utilities.mt3}>Để nắm rõ hơn về tình hình loại bệnh, bạn có thể bấm qua tab tips hoặc tìm đến bác sĩ</Text>
+              </View>
+            )}
       </View>
       <TouchableOpacity onPress={() => navigation.navigate("Intro")}>
         <Text style={[buttons.btn, buttons.bottomBtn, buttons.roundBtn]}>
